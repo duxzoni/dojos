c985f5d529d657bc3051543a4692647be0b7419f
Eduardo Ghizoni <duxzoni@gmail.com>
2019-02-26 11:35:41 -0300

    caixa eletronico

---
+++ src/main/java/br/ghizoni/dojo/caixaeletronico/Caixa.java
@@ -0,0 +1,32 @@
+package caixa;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Caixa {
+    public Map<Integer, Integer> saque(Integer valor) {
+
+        if (valor == null || valor % 10 != 0){
+            throw new IllegalArgumentException("Não pode notas não divisiveis por 10 ou saque nulo");
+
+        }
+        Map<Integer, Integer> listaNotas = new HashMap<>();
+
+        int resto = valor;
+
+        resto = getResto(valor, listaNotas, resto, 100);
+        resto = getResto(resto, listaNotas, resto, 50);
+        resto = getResto(resto, listaNotas, resto, 20);
+        resto = getResto(resto, listaNotas, resto, 10);
+
+        return listaNotas;
+    }
+
+    private int getResto(Integer valor, Map<Integer, Integer> listaNotas, int resto, int cem) {
+        if(valor >= cem) {
+            resto = valor % cem;
+            listaNotas.put(cem, valor / cem);
+        }
+        return resto;
+    }
+}
---
+++ src/test/java/br/ghizoni/dojo/caixaeletronico/CaixaTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.Assert.*;
+
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.verify;
+
+public class CaixaTest {
+
+}
